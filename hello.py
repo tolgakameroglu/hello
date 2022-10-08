ceb007b9003f5e0c2290da3955b8d0bc0a2906b5
tolgakameroglu <tolga_kameroglu@hotmail.com>
2022-10-08 17:30:56 +0300

    dfrg

---
+++ hello.py
@@ -0,0 +1,48 @@
+
+
+""" name ="Tolga"
+
+print(name[0])
+
+# Define a list
+names=["tolga","neso","ipo","nazo"]
+print(names)
+print(names[0])
+names.append("deneme")
+print(names)
+names.sort()
+print(names)
+
+
+#Create Set
+s = set()
+s.add("tolga")
+s.add("neso")
+s.add("nazo")
+s.add("ipo")
+s.add(1)
+# no twice element in set
+s.add(1)
+print(s)
+s.remove(1)
+print(s)
+print(F"the set has {len(s)} elements")
+
+"""
+
+
+
+
+n = int(input("Number : "))
+
+if n>0 :
+    print("n is positive")
+elif n<0:
+    print("n is negative")
+else:
+    print("n is zero")
+    print("deneme1")
+
+print("deneme3")
+
+
